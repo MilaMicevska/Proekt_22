c6a644c16cf9ef5ea2862b6d0134256398a0031c
MilaMicevska <micevska.mila@gmail.com>
2023-12-26 13:43:12 +0100

    p22

---
+++ src/proekt22/Kuce.java
@@ -0,0 +1,21 @@
+package proekt22;
+
+public class Kuce {
+  public void osobina () {
+	  System.out.println("Kuceto lae");
+  }
+}
+class LoveckoKuce extends Kuce {
+	public void osobina () {
+		System.out.println ("Loveckoto Kuce dusku");
+	}
+}
+class glavna {
+	public static void main (String []args) {
+		Kuce reks = new Kuce ();
+		reks.osobina ();
+		LoveckoKuce lovec=new
+				lovec.osobina ();
+		
+	}
+	}
